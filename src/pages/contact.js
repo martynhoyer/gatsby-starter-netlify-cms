be37fcd1bb6820bac1b42a11c8e574981786bc0f
Martyn Hoyer <martyn.hoyer@ticketmaster.co.uk>
2018-02-12 22:23:39 +0000

    Add form state

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,7 +9,11 @@ function encode(data) {
 export default class Contact extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      loading: false,
+      submitted: false,
+      error: false
+    };
   }
 
   handleChange = e => {
@@ -17,13 +21,14 @@ export default class Contact extends React.Component {
   };
 
   handleSubmit = e => {
+    this.setState({ loading: true });
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...this.state })
     })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
+      .then(() => this.setState({ submitted: true, loading: false }))
+      .catch(error => this.setState({ error: true, loading: false }));
 
     e.preventDefault();
   };
@@ -32,41 +37,45 @@ export default class Contact extends React.Component {
     return (
       <div>
         <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          <p hidden>
-            <label>
-              Don’t fill this out: <input name="bot-field" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:<br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+        {!this.state.submitted ? (
+          <form
+            name="contact"
+            method="post"
+            action="/thanks/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={this.handleSubmit}
+          >
+            <p hidden>
+              <label>
+                Don’t fill this out: <input name="bot-field" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your name:<br />
+                <input type="text" name="name" onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your email:<br />
+                <input type="email" name="email" onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message:<br />
+                <textarea name="message" onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        ) : (
+          <div>Thanks!</div>
+        )}
       </div>
     );
   }

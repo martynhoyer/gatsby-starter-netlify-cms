418ed3cbb7dba3892e78c17be452c84246291efe
Martyn Hoyer <martyn.hoyer@ticketmaster.co.uk>
2018-02-18 15:17:14 +0000

    Update CMS for services home page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,12 @@ import CMS from "netlify-cms";
 import "netlify-cms/dist/cms.css";
 
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import ServicesHomePagePreview from "./preview-templates/ServicesHomePagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import ProductPagePreview from "./preview-templates/ProductPagePreview";
 
 CMS.registerPreviewStyle("/styles.css");
 CMS.registerPreviewTemplate("about-us", AboutPagePreview);
+CMS.registerPreviewTemplate("services-home", ServicesHomePagePreview);
 CMS.registerPreviewTemplate("products", ProductPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);

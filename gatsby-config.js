f9b420d7f68bb9ce9cd0aa13867528e7453c4b4d
Martyn Hoyer <martyn.hoyer@ticketmaster.co.uk>
2018-02-24 21:43:29 +0000

    fix netlify-cms issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-netlify-cms",
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -25,12 +26,6 @@ module.exports = {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: []
-      }
-    },
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`
       }
     },
     // {

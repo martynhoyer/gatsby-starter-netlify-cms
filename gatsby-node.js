870c91e1903d8c77942411af4108864138e03fe9
Martyn Hoyer <martyn.hoyer@ticketmaster.co.uk>
2018-03-13 22:07:56 +0000

    Add slug to node config as per starter

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,20 @@
 const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
 
-exports.createPages = ({
-  boundActionCreators,
-  graphql
-}) => {
-  const {
-    createPage
-  } = boundActionCreators;
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators;
 
   return graphql(`
     {
       allMarkdownRemark(limit: 1000) {
         edges {
           node {
+            id
+            fields {
+              slug
+            }
             frontmatter {
               templateKey
-              path
             }
           }
         }
@@ -27,18 +26,31 @@ exports.createPages = ({
       return Promise.reject(result.errors);
     }
 
-    return result.data.allMarkdownRemark.edges.forEach(({
-      node
-    }) => {
-      const pagePath = node.frontmatter.path
+    result.data.allMarkdownRemark.edges.forEach(edge => {
+      const id = edge.node.id;
       createPage({
-        path: pagePath,
+        path: edge.node.fields.slug,
         component: path.resolve(
-          `src/templates/${String(node.frontmatter.templateKey)}.js`
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
         ),
         // additional data can be passed via context
-        context: {},
+        context: {
+          id
+        }
       });
     });
   });
+};
+
+exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
+  const { createNodeField } = boundActionCreators;
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: `slug`,
+      node,
+      value
+    });
+  }
 };

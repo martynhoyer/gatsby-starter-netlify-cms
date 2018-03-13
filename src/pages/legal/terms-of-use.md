1f7726e4a1a4c13ec1a1490e1e7b71f3030ec795
Martyn Hoyer <martyn.hoyer@ticketmaster.co.uk>
2018-03-13 21:31:36 +0000

    Update legal page template and sample page content

--- src/pages/legal/terms-of-use.md
+++ src/pages/legal/terms-of-use.md
@@ -4,9 +4,3 @@ path: /terms-of-use
 title: Terms of use
 date: 2018-03-10T00:00:00+00:00
 ---
-
-We don't subscribe to having a immovable paper plan or an over-arching company ethos. However we do think you need to understand your customers, your competitors and where you are all headed.
-
-Corse Concierge has its roots deep in the highly competitive world of motorsport. Racing teams are always seeking a competitive advantage. We can help you do the same with your business. We work with businesses at the start of their journey and established organisations looking to turbo charge current growth.
-
-At Corse Concierge we don't believe you have to do the same as everyone else to be successful. Nor that you have to have the biggest budget. Once we understand your strengths and the solutions you can offer, you can use our market knowledge and business know-how to guide you.
---
+++ src/templates/legal.js
@@ -0,0 +1,41 @@
+import React from "react";
+import graphql from "graphql";
+import Link from "gatsby-link";
+import Content, { HTMLContent } from "../components/Content";
+import SeoTitle from "../components/SeoTitle";
+
+export const LegalPageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content;
+
+  return (
+    <div>
+      <PageContent className="content" content={content} />
+    </div>
+  );
+};
+
+export default ({ data }) => {
+  const { markdownRemark: page } = data;
+  return (
+    <main>
+      <SeoTitle title={page.frontmatter.title} />
+      <LegalPageTemplate
+        contentComponent={HTMLContent}
+        title={page.frontmatter.title}
+        content={page.html}
+      />
+    </main>
+  );
+};
+
+export const legalPageQuery = graphql`
+  query LegalPage($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      frontmatter {
+        title
+        path
+      }
+      html
+    }
+  }
+`;

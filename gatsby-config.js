48deb46591d221c9ea2a8d84f812599282276497
Martyn Hoyer <martyn.hoyer@ticketmaster.co.uk>
2018-04-05 19:04:51 +0100

    Add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-catch-links",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-netlify",
     "gatsby-plugin-netlify-cms",
     {
       resolve: `gatsby-plugin-sitemap`
